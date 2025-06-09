0422c091647f40784b8a25028f2d0f54e3794994
Jordi Cantó Gálvez <developsuricata@gmail.com>
2025-06-09 19:16:30 +0200

    Add Base OptoScale Experience Data

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -7,6 +7,15 @@ export interface ExperienceData {
     labels: string[];
 }
 
+export const optoscaleData: ExperienceData = {
+    period: "Jun 2025 - Present",
+    role: "Full-Stack Developer",
+    company: "OptoScale",
+    companyLink: "https://optoscale.no/?lang=en",
+    roleFunctions: ["Font-End Development", "Back-End Development"],
+    labels: ["React", "TypeScript", "NestJS", "Prisma", "PostgreSQL", "Docker"],
+};
+
 export const trackingplanData: ExperienceData = {
     period: "Dec 2021 - Apr 2025",
     role: "Full-Stack Developer",
@@ -18,9 +27,19 @@ export const trackingplanData: ExperienceData = {
         "Code Review - Active participation in code reviews, committed to maintaining high quality standards and promoting best practices within the development team.",
         "Deploy Management - Responsibility for managing automatic deploys using AWS CodePipeline, ensuring continuous and stable deliveries in both development and production environments.",
         "Interdisciplinary Collaboration - Close collaboration with development and UX/UI design teams, ensuring that technical functionalities align with user experience objectives.",
-        "On-call Rotation - Responsibility for reporting, tracking, and resolving errors, adopting a proactive approach to maintain system stability and reliability. Collaboration with various departments to facilitate quick resolution of incidents, thus contributing to product quality and user satisfaction."
+        "On-call Rotation - Responsibility for reporting, tracking, and resolving errors, adopting a proactive approach to maintain system stability and reliability. Collaboration with various departments to facilitate quick resolution of incidents, thus contributing to product quality and user satisfaction.",
     ],
-    labels: ["React", "TypeScript", "Cypress", "Python", "AWS", "Clickhouse", "SQL", "GitHub", "Notion"]
+    labels: [
+        "React",
+        "TypeScript",
+        "Cypress",
+        "Python",
+        "AWS",
+        "Clickhouse",
+        "SQL",
+        "GitHub",
+        "Notion",
+    ],
 };
 
 export const prensalinkData: ExperienceData = {
@@ -31,9 +50,20 @@ export const prensalinkData: ExperienceData = {
     roleFunctions: [
         "Software Development and Product Management Collaboration - Participation in software development alongside Product Management, involving activities such as planning, coding, and design reviews. This collaboration ensures that development efforts are aligned with product goals and user needs.",
         "E-commerce Platform Development, Deployment, and Maintenance - Participation in the development, deployment, and maintenance of an e-commerce platform for link building utilizing technologies like React, Laravel, Docker, Bitbucket as the code repository, and JIRA for task management.",
-        "Cloud Infrastructure Maintenance - Involvement in maintaining a cloud-based infrastructure for deploying all company products, using technologies such as Amazon EC2 for web servers, S3 for nightly backups."
+        "Cloud Infrastructure Maintenance - Involvement in maintaining a cloud-based infrastructure for deploying all company products, using technologies such as Amazon EC2 for web servers, S3 for nightly backups.",
     ],
-    labels: ["React", "Node.js", "TypeScript", "Laravel", "PHP", "Docker", "AWS", "Google Cloud", "Jira", "Bitbucket"]
+    labels: [
+        "React",
+        "Node.js",
+        "TypeScript",
+        "Laravel",
+        "PHP",
+        "Docker",
+        "AWS",
+        "Google Cloud",
+        "Jira",
+        "Bitbucket",
+    ],
 };
 
 export const gttData: ExperienceData = {
@@ -44,7 +74,7 @@ export const gttData: ExperienceData = {
     roleFunctions: [
         "Application Development and Maintenance - Engaged in the development and ongoing maintenance of the Tax Application, primarily using PL/SQL. This role underscores a deep understanding of database management and programming techniques to ensure the application’s reliability and performance.",
         "Module Collaboration - Actively collaborated across different modules within the organization, facilitating integration and cohesive functionality within the Tax Information System. This collaboration ensured that the system met comprehensive needs and functional requirements effectively.",
-        "Rapid Learning and Adaptation - Demonstrated rapid learning and adaptation in a complex tax information system environment, starting from scratch and quickly becoming proficient in PL/SQL and related technologies. Contributed effectively to the team by applying newly acquired skills to solve problems and enhance system functionality."
+        "Rapid Learning and Adaptation - Demonstrated rapid learning and adaptation in a complex tax information system environment, starting from scratch and quickly becoming proficient in PL/SQL and related technologies. Contributed effectively to the team by applying newly acquired skills to solve problems and enhance system functionality.",
     ],
-    labels: ["C#", "ASP.NET", "PL/SQL", "Oracle"]
+    labels: ["C#", "ASP.NET", "PL/SQL", "Oracle"],
 };
